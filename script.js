85af3c65f9df3c41ec43911a2a3e8b4e806b4856
henrylacava <lacavahenry@gmail.com>
2023-05-23 15:10:15 -0300

    fix div

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
     loop: true
   }).pause(2000).go()
 })
+
+
+const arrow = document.querySelector('.setaBaixo');
+arrow.addEventListener('click', () => {
+  window.scroll({
+    top: 550,
+    behavior:'smooth',
+  });
+})
